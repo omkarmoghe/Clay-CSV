81a96796574db4a1bf05ac4440940d1de28d2259
Omkar Moghe <theomkarmoghe@gmail.com>
2023-11-13 00:20:21 -0800

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TableViewEditorProvider } from './editors/tableViewEditorProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.debug("clay-csv.active");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "clay-csv" is now active!');
+	const tableViewEditorProvider = new TableViewEditorProvider(context);
+	const tableViewEditorDisposable = vscode.window.registerCustomEditorProvider(
+		TableViewEditorProvider.VIEW_TYPE,
+		tableViewEditorProvider
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('clay-csv.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Clay CSV!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(tableViewEditorDisposable);
 }
 
 // This method is called when your extension is deactivated
